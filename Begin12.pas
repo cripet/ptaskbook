95916c8f9df436d357e4cef40f0db48a494db3e2
cripet <ppcristea@gmail.com>
2020-01-23 22:00:22 +0200

    Add files via upload

---
+++ Begin12.pas
@@ -0,0 +1,29 @@
+﻿//[RO] Begin12°. Sunt date 2 catete ale unui triunghi dreptunghic. Sa se gaseasca ipotenuza c  si perimetrul P al acestuia: c = (a2 + b2)1/2, P = a + b + c.
+//[EN] Begin12°. The legs a and b of a right triangle are given. Find the hypotenuse c and the perimeter P of the triangle: c = (a2 + b2)1/2, P = a + b + c.
+//[RU] Begin12°. Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P: c = (a2 + b2)1/2, P = a + b + c.
+
+
+begin
+  var a, b, c, p: real;
+  
+  repeat
+    begin
+      writeln('Introduceti un numar pozitiv:');
+      readln(a);
+      if a <= 0 then writeln('Numarul trebuie sa fie pozitiv!');
+    end;
+  until (a > 0);
+  
+  repeat
+    begin
+      writeln('Introduceti un alt numar pozitiv:');
+      readln(b);
+      if b <= 0 then writeln('Numarul trebuie sa fie pozitiv!');
+    end;
+  until (b > 0);
+  
+  c := sqrt(a * a + b * b);  // abs = absolute value = valoarea absoluta = modul
+  p := a + b + c;
+  writeln('Ipotenuza = ', c);
+  writeln('Perimetrul = ', p); 
+end.
---
+++ Begin14.pas
@@ -0,0 +1,23 @@
+﻿//[RO] Begin14°. Fiind data lungimea L a circumferintei, sa se gaseasca  raza R si aria S a cercului. L = 2·π·R,    S = π·R2. Utilizitati 3.14 pentru π. 
+//[EN] Begin14°. Given the length L of a circumference, find the radius R and the area S of the circle. Take into account that L = 2·π·R,    S = π·R2. Use 3.14 for a value of π. 
+//[RU] Begin14°. Дана длина L окружности. Найти ее радиус R и площадь S круга, ограниченного этой окружностью, учитывая, что L = 2·π·R,    S = π·R2. В качестве значения π использовать 3.14. 
+
+const
+  pi = 3.14;
+
+begin
+  var l, r, s: real;
+  
+  repeat
+    begin
+      writeln('Introduceti lungimea cercului:');
+      readln(l);
+      if l <= 0 then writeln('Numarul trebuie sa fie pozitiv!');
+    end;
+  until (l > 0);
+  
+  r := l / (2 * pi);
+  s := pi * r * r;
+  writeln('Raza = ', r:0:2);
+  writeln('Aria = ', s:0:2);
+end.
