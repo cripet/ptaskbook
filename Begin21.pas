f1a466834014a8e53a3d0f0a3153dad89cfffd40
cripet <ppcristea@gmail.com>
2020-02-01 19:46:11 +0200

    Add files via upload

---
+++ Begin21.pas
@@ -0,0 +1,58 @@
+﻿//Begin21°. Sunt date coordonatele celor 3 varfuri (x1, y1), (x2, y2), (x3, y3) ale unui triunghi. 
+//          Sa se gaseasca perimetrul si aria triunghiului folosind formula pentru calcularea distantei dintre 
+//          doua puncte in spatiu (vedeti Begin20). 
+//          Aria triunghiului cu laturile a, b, c poate fi calculata dupa formula lui Heron: S = (p·(p − a)·(p − b)·(p − c))1/2, 
+//          unde p = (a + b + c)/2 este jumatate din perimetrul triunghiului. 
+//Begin21°. The coordinates (x1, y1), (x2, y2), (x3, y3) of the triangle vertices are given. 
+//          Find the perimeter and the area of the triangle using the formula for distance between 
+//          two points in the plane (see Begin20). The area of a triangle with sides a, b, c 
+//          can be found by Heron formula: S = (p·(p − a)·(p − b)·(p − c))1/2, where p = (a + b + c)/2 is the half-perimeter. 
+//Begin21°. Даны координаты трех вершин треугольника: (x1, y1), (x2, y2), (x3, y3). 
+//          Найти его периметр и площадь, используя формулу для расстояния между двумя точками на 
+//          плоскости (см. задание Begin20). Для нахождения площади треугольника со сторонами a, b, c 
+//          использовать формулу Герона: S = (p·(p − a)·(p − b)·(p − c))1/2, где p = (a + b + c)/2 — полупериметр. 
+
+// S = (p·(p − a)·(p − b)·(p − c))1/2
+function obtine_aria_triunghi(a, b, c: real): real;
+begin
+  var p: real;
+  p := (a + b + c) / 2;
+  obtine_aria_triunghi := sqrt(p * (p - a) * (p - b) * (p - c));
+end;
+
+function obtine_distanta_2_puncte(x1, y1, x2, y2: real): real;
+begin
+  obtine_distanta_2_puncte := sqrt(power(x2 - x1, 2) + power(y1 - y2, 2));
+end;
+
+begin
+  var x1, x2, x3, y1, y2, y3, p, s, a, b, c: real;
+    // 1. Introducem variabilele de la tastatura
+  writeln('Introduceti coordonata X1:');
+  readln(x1);
+  writeln('Introduceti coordonata Y1:');
+  readln(y1);
+  writeln('Introduceti coordonata X2:');
+  readln(x2);
+  writeln('Introduceti coordonata Y2:');
+  readln(y2);
+  writeln('Introduceti coordonata X3:');
+  readln(x3);
+  writeln('Introduceti coordonata Y3:');
+  readln(y3);
+  
+  // 2. Calculam laturile triunghiului
+  a := obtine_distanta_2_puncte(x1, y1, x2, y2);
+  b := obtine_distanta_2_puncte(x2, y2, x3, y3);
+  c := obtine_distanta_2_puncte(x1, y1, x3, y3);
+  
+  // 3. Calculam perimetrul triunghiului
+  p := a + b + c;
+  
+  // 4. Calculam aria triunghiului
+  s := obtine_aria_triunghi(a, b, c);
+  
+  // 5. Afisam rezultatul la ecran
+  writeln('Perimetrul triunghiului = ', p:0:2);
+  writeln('Aria triunghiului = ', s:0:2);
+end.
---
+++ Begin24.pas
@@ -0,0 +1,31 @@
+﻿// Begin24°. Sunt date variabilele A, B, C. Schimbati valorile acestora dandu-i valoarea lui A lui C, 
+//           valoarea lui C lui B, iar valoarea lui B lui A. Afisati la ecran noile valori.
+// Begin24°. Variables A, B, C are given. Change values of the variables by moving the given value of A 
+//           into the variable C, the given value of C into the variable B, and the given value of B into the variable A. 
+//           Output the new values of A, B, C. 
+// Begin24°. Даны переменные A, B, C. Изменить их значения, переместив содержимое A в C, C — в B, B — в A, 
+//           и вывести новые значения переменных A, B, C. 
+
+// A -> C
+// B -> A
+// C -> B
+
+begin
+  var a, b, c, c2 : real;
+  writeln('Introduceti valoarea lui A:');
+  readln(a);
+  writeln('Introduceti valoarea lui B:');
+  readln(b);
+  writeln('Introduceti valoarea lui C:');
+  readln(c);
+    
+  c2 := c;
+  
+  c := a;  
+  a := b;
+  b := c2;
+ 
+  writeln('A = ', a);
+  writeln('B = ', b);
+  writeln('C = ', c);
+end.
