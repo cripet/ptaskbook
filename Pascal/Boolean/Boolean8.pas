31179360b31ff6c7621e76abf7aab4d62a44329e
cripet <ppcristea@gmail.com>
2020-02-21 18:55:46 +0200

    Add files via upload

---
+++ Pascal/Boolean/Boolean8.pas
@@ -0,0 +1,12 @@
+﻿//[RO] Boolean8°. Sunt date 2 numere intregi A si B. Sa se verifice urmatoarea expresie: 
+//                "Ambele numere A si B sunt impare".
+//[EN] Boolean8°. Given two integers A and B, verify the following proposition: "Each of the numbers A and B is odd".
+
+
+begin
+  var a, b : integer;
+  a := 6;
+  b := 7;
+  
+  writeln('Ambele numere A si B sunt impare  => ', (a mod 2 = 1) and (b mod 2 = 1) );
+end.
---
+++ Pascal/Boolean/Boolean9.pas
@@ -0,0 +1,12 @@
+﻿//[RO] Boolean9°. Sunt date 2 numere intregi A si B. Sa se verifice urmatoarea expresie: 
+//                "Cel putin un numar este impar".
+//[EN] Boolean9°. Given two integers A and B, verify the following proposition: "At least one of the numbers A and B is odd".
+
+
+begin
+  var a, b : integer;
+  a := 6;
+  b := 8;
+  
+  writeln('Cel putin un numar este impar  => ', (a mod 2 = 1) or (b mod 2 = 1) );
+end.
